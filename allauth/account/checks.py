4a87fa33a507f3ba32ca733ad11f161399f0ef4b
vaghaniamit <vaghaniamit4741@gmail.com>
2026-03-11 19:07:09 +0530

    EMAIL_VERIFICATION_BY_CODE_ENABLED check is dissabled so we don't need to skip check during server run

--- allauth/account/checks.py
+++ allauth/account/checks.py
@@ -41,16 +41,16 @@ def settings_check(app_configs, **kwargs):
                     msg="SOCIALACCOUNT_ONLY requires ACCOUNT_EMAIL_VERIFICATION = 'none'"
                 )
             )
-    if (
-        app_settings.EMAIL_VERIFICATION_BY_CODE_ENABLED
-        and app_settings.EMAIL_VERIFICATION
-        != app_settings.EmailVerificationMethod.MANDATORY
-    ):
-        ret.append(
-            Critical(
-                msg="ACCOUNT_EMAIL_VERFICATION_BY_CODE requires ACCOUNT_EMAIL_VERIFICATION = 'mandatory'"
-            )
-        )
+    # if (
+    #     app_settings.EMAIL_VERIFICATION_BY_CODE_ENABLED
+    #     and app_settings.EMAIL_VERIFICATION
+    #     != app_settings.EmailVerificationMethod.MANDATORY
+    # ):
+    #     ret.append(
+    #         Critical(
+    #             msg="ACCOUNT_EMAIL_VERFICATION_BY_CODE requires ACCOUNT_EMAIL_VERIFICATION = 'mandatory'"
+    #         )
+    #     )
 
     # Often made mistake: ACCOUNT_SIGNUP_FIELDS = [..., "password", ...]
     signup_fields = getattr(settings, "ACCOUNT_SIGNUP_FIELDS", None)
